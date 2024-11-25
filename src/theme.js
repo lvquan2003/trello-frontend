430fa27d0b206456e766e2b1bdc7051db599fa36
lvquan2003 <levanquan051003@gmail.com>
2024-11-26 02:41:01 +0700

    refactor Project structure

--- src/theme.js
+++ src/theme.js
@@ -1,9 +1,13 @@
 import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
-import { teal, deepOrange, cyan, orange  } from '@mui/material/colors'
+import { teal, deepOrange, cyan, orange } from '@mui/material/colors'
 
 
 // Create a theme instance.
 const theme = extendTheme({
+  trello: {
+    appBarHeight: '48px',
+    boardBarHeight: '58px'
+  },
   colorSchemes: {
     light: {
       palette: {
