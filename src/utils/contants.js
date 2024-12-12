cff0332ec065a94eecfd908b9ee673cf4e80720d
lvquan2003 <levanquan051003@gmail.com>
2024-12-12 14:04:26 +0700

    fix BUILD_MODE dev and production

--- src/utils/contants.js
+++ src/utils/contants.js
@@ -1,1 +1,10 @@
-export const API_ROOT = 'http://localhost:8017'
+let apiRoot = ''
+
+if (process.env.BUILD_MODE === 'dev') {
+  apiRoot = 'http://localhost:8017'
+}
+
+if (process.env.BUILD_MODE === 'production') {
+  apiRoot = 'https://trello-backend-11ar.onrender.com/'
+}
+export const API_ROOT = apiRoot
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 import viteSvgr from 'vite-plugin-svgr'
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    'process.env': process.env
+  },
   plugins: [react(), viteSvgr()],
   resolve: {
     alias: [
